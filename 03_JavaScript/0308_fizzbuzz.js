5622d6013d4b7af06b0ea9a3db87f51a8249cc28
shinichiyamada <yamada@fbicenter.co.jp>
2019-01-08 19:38:50 +0900

    update: 表記修正 説明追加

--- 03_JavaScript/0308_fizzbuzz.js
+++ 03_JavaScript/0308_fizzbuzz.js
@@ -3,7 +3,9 @@ var NUM = 100 + 1
 
 console.log("")
 console.log(`-- case1 -----------------`)
+console.log(`素直な方法 おそらく一番見る`)
 
+var start_ms = new Date().getTime()
 for (var i = 1; i < NUM; i++) {
   if (i % 15 === 0) {
     console.log("fizzbuzz")
@@ -18,7 +20,9 @@ for (var i = 1; i < NUM; i++) {
 
 console.log("")
 console.log(`-- case2 -----------------`)
+console.log(`素直な方法2 ここまでは調べれば出てくる`)
 
+start_ms = new Date().getTime()
 for (var i = 1; i < NUM; i++) {
   var output = ""
   if (i % 3 === 0) {
@@ -36,7 +40,11 @@ for (var i = 1; i < NUM; i++) {
 
 console.log("")
 console.log(`-- case3 -----------------`)
+console.log(`ルールを変数一つで表現しようというアプローチ`)
+console.log(`ロジック上は最速候補で単純な計算だけならcase1の1/2倍、case2の1/30程度速い(出力処理が10000倍程度遅いので誤差の範囲)`)
+console.log(`1~15で一巡するのであらかじめ決めうちしておく 個人的にお気に入り`)
 
+start_ms = new Date().getTime()
 var MAP = [
   "", "", "fizz", "", "buzz",
   "fizz", "", "", "fizz", "buzz",
@@ -55,6 +63,8 @@ for (var i = 1; i < NUM; i++) {
 
 console.log("")
 console.log(`-- case4 -----------------`)
+console.log(`特に意味のない再帰と特に意味のないswitch 最遅の上NUMが多いと落ちる`)
+console.log(`一応マルチスレッドにも対応できる利点がある 例えばconsole.logがファイルの非同期出力でも(少し書き換えれば)正しく出力できる`)
 
 function fizzbuzz(n, end) {
 
